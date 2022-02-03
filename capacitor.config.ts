eaeb77a58032bafa6c2015981bdf3adb9f152b07
calebvirayyy <98898510+calebvirayyy@users.noreply.github.com>
2022-02-04 07:56:37 +0800

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Tagalog Word Puzzle Game',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
